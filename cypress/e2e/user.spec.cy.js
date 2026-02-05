1ae1098e96e7f9dd7b86b91ef67ed153545d04d2
Sonny Anderson <sonyanderson05@hotmail.com>
2026-02-04 21:16:40 -0300

    Interagindo com caixa de selecao

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -18,7 +18,9 @@ describe('Orange HRM Tests', () => {
     genericField: ".oxd-input--active",
     dateField: "[placeholdder='yyyy-mm-dd']",
     dateCloseBotton: ".--close",
-    submitBotton: "[type='submit']"
+    submitBotton: "[type='submit']",
+    buttonselect: ".oxd-select-text--arrow",
+    selectsuspense: "[class='oxd-icon bi-caret-down-fill oxd-select-text--arrow']"
   }
 
   it.only('User Info Update - Sucesso', () => {
@@ -32,17 +34,20 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorsList.middleNameField).clear().type('MiddleNameTest')
     cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(2).clear().type('NickNameTest')
+    //cy.get(selectorsList.genericField).eq(2).clear().type('NickNameTest')
     cy.get(selectorsList.genericField).eq(3).clear().type('Emp.Idtest')
     cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('DLNTest')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2026-08-29')
+    //cy.get(selectorsList.genericField).eq(6).clear().type('DLNTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2026-08-29')
     cy.get(selectorsList.dateCloseBotton).click()
+    cy.get(selectorsList.buttonselect).eq(0).click()
+    cy.get(selectorsList.selectsuspense).eq(0).click()
     cy.get(selectorsList.submitBotton).eq(0).click()
     cy.get('body').should('contain', 'Successfully')
     cy.get('.oxd-toast-close')
     //cy.get(selectorsList.genericfield).eq(9).type('Test')
     //cy.get(selectorsList.genericfield).eq(10).type('Test')
+    
   })
   it('login - Fail', () => {
     cy.visit('/auth/login')
