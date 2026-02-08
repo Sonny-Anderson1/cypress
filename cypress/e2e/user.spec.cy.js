a35dd223ae2a03e684435b49173493296caff3c8
Sonny Anderson <sonyanderson05@hotmail.com>
2026-02-08 12:46:55 -0300

    Using pages for logins

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,16 +1,14 @@
 import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPage.js'
 
+const loginPage = new LoginPage()
 
 describe('Orange HRM Tests', () => {
 
 
   const selectorsList ={
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type= 'submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
     dasboardGrid: ".orangehrm-dashboard-grid",
-    wrongcredentialAlert: "[role='alert']",
     myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
@@ -24,29 +22,29 @@ describe('Orange HRM Tests', () => {
   }
 
   it.only('User Info Update - Sucesso', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dasboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.middleNameField).clear().type('MiddleNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    //cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    //cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    //cy.get(selectorsList.loginButton).click()
+    //cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    //cy.get(selectorsList.dasboardGrid)
+    //cy.get(selectorsList.myInfoButton).click()
+    //cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    //cy.get(selectorsList.middleNameField).clear().type('MiddleNameTest')
+    //cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
     //cy.get(selectorsList.genericField).eq(2).clear().type('NickNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('Emp.Idtest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    //cy.get(selectorsList.genericField).eq(3).clear().type('Emp.Idtest')
+    //cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
     //cy.get(selectorsList.genericField).eq(6).clear().type('DLNTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('2026-08-29')
-    cy.get(selectorsList.dateCloseBotton).click()
-    cy.get(selectorsList.buttonselect).eq(0).click()
-    cy.get(selectorsList.selectsuspense).eq(0).click()
-    cy.get(selectorsList.submitBotton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully')
-    cy.get('.oxd-toast-close')
-    //cy.get(selectorsList.genericfield).eq(9).type('Test')
-    //cy.get(selectorsList.genericfield).eq(10).type('Test')
+    //cy.get(selectorsList.genericField).eq(6).clear().type('2026-08-29')
+    //cy.get(selectorsList.dateCloseBotton).click()
+    //cy.get(selectorsList.buttonselect).eq(0).click()
+    //cy.get(selectorsList.selectsuspense).eq(0).click()
+    //cy.get(selectorsList.submitBotton).eq(0).click()
+    //cy.get('body').should('contain', 'Successfully')
+    //cy.get('.oxd-toast-close')
     
   })
   it('login - Fail', () => {
